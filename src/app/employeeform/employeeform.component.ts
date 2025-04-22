f37ecef3cb370a860103f1c95eb24a0a3960541a
Thirisha-M <thirisha.m20@gmail.com>
2025-04-22 15:42:35 +0530

    chore: save changes before Angular upgrade

--- src/app/employeeform/employeeform.component.ts
+++ src/app/employeeform/employeeform.component.ts
@@ -9,26 +9,23 @@ import { Employee } from '../employeeform_Model';
 export class EmployeeformComponent {
   employees: Employee[] = [];
   newEmployee: Employee = { id: 0, name: '', salary: 0, fieldOfWork: '' };
+
   addEmployee() {
-  
-  if (this.newEmployee.name && this.newEmployee.salary > 0 &&
-  this.newEmployee.fieldOfWork) {
-  this.employees.push({ ...this.newEmployee });
-  this.resetForm();
+    if (
+      this.newEmployee.name &&
+      this.newEmployee.salary > 0 &&
+      this.newEmployee.fieldOfWork
+    ) {
+      this.employees.push({ ...this.newEmployee });
+      this.resetForm();
+    }
   }
-  }
+
   deleteEmployee(id: number) {
-  this.employees = this.employees.filter(employee => employee.id !==
-  id);
+    this.employees = this.employees.filter(employee => employee.id !== id);
   }
+
   resetForm() {
-  this.newEmployee = { id: 0, name: '', salary: 0, fieldOfWork: '' };
+    this.newEmployee = { id: 0, name: '', salary: 0, fieldOfWork: '' };
   }
 }
-this.resetForm();
-deleteitems(Itemid: number) {
-  this.items = this.items.filter(Itemid=> newitems.Itemid !== Itemid);
-  }
-  resetForm() {
-  this.newitems = { Itemid: 0, Quantity:0, Additem: 0 };
-  }
